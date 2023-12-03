ea5a831f3b9b44b7e506995149dc9f814f1c3141
Andrey Belykh <andrejbelykh11@gmail.com>
2023-12-03 21:07:24 +0100

    Deleted one stroke

--- bot-json.py
+++ bot-json.py
@@ -69,9 +69,6 @@ def welcome(message):
         bot.register_next_step_handler(message, welcome)
 
 
-users_choice_list0 = ["POST", "GET", 'post', 'get', 'DELETE', 'UPDATE', 'Delete', 'Update', 'delete', 'update', 'Post',
-                      'Get']
-
 
 @bot.message_handler(commands=["POST", "GET", 'post', 'get', 'DELETE', 'UPDATE', 'Delete', 'Update', 'delete', 'update', 'Post', 'Get'])
 def choice_log_postget(message):
